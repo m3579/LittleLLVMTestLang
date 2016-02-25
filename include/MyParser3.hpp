e6bbde36756e8f81b090fb57eddee4f452b360ea
M3579 <mihirkasmalkar@gmail.com>
2016-02-24 18:54:39 -0800

    Working on implementing optional constructs and logical OR's in construct's token types

--- include/MyParser3.hpp
+++ include/MyParser3.hpp
@@ -55,30 +55,39 @@ Parser getParser(Lexer lexer)
     Parser myParser(lexer);
 
     // let
-    SP<Construct> let(new Construct("Keyword - let", TType::Keyword::LET, NType::Keyword::LET, 0, 0));
+    SP<Construct> let_keyword(new Construct("Keyword - let", TType::Keyword::LET, NType::Keyword::LET, 0, 0, false));
+
+    // <space>
+    SP<Construct> spaceAfterLet(new Construct("Space After Let", TType::SPACE, NType::SPACE, 0, 0, false));
+
+    // let + <space>
+    std::vector<SP<Construct>> letKeywordAndSpaceComponents {
+        let_keyword, spaceAfterLet
+    };
+    SP<Construct> letKeywordAndSpace(new Construct("Keyword - let + Space After Let", letKeywordAndSpaceComponents, 0, 0, true));
 
     // var
-    SP<Construct> identifier(new Construct("Identifier", TType::IDENTIFIER, NType::IDENTIFIER, 0, 0));
+    SP<Construct> identifier(new Construct("Identifier", TType::IDENTIFIER, NType::IDENTIFIER, 0, 0, false));
 
     // =
-    SP<Construct> assignment_op(new Construct("Operator - assignment", TType::Operator::ASSIGNMENT_OP, NType::Operator::ASSIGNMENT_OP, 0, 0));
+    SP<Construct> assignment_op(new Construct("Operator - assignment", TType::Operator::ASSIGNMENT_OP, NType::Operator::ASSIGNMENT_OP, 0, 0, false));
     assignment_op->notFound = [] (TokenManager& tm) {
         std::cout << "Error: expected assignment operator\n";
         tm.exit = true;
     };
 
     // value
-    SP<Construct> value(new Construct("Value", TType::Value::NUMBER, NType::Value::NUMBER, 0, 0));
+    SP<Construct> value(new Construct("Value", TType::Value::NUMBER, NType::Value::NUMBER, 0, 0, false));
 
     // <space>
-    SP<Construct> space(new Construct("Space", TType::SPACE, NType::SPACE, 0, 0));
+    SP<Construct> space(new Construct("Space", TType::SPACE, NType::SPACE, 0, 0, false));
 
     // Variable declaration statement
     std::vector<SP<Construct>> var_dec_components {
-        let, space, identifier, space, assignment_op, space, value
+        letKeywordAndSpace, identifier, space, assignment_op, space, value
     };
 
-    SP<Construct> var_dec_constr(new Construct("Variable Declaration", var_dec_components, 0, 0));
+    SP<Construct> var_dec_constr(new Construct("Variable Declaration", var_dec_components, 0, 0, false));
 
     SP<ConstructTreeFormNode> var_dec_treeForm(new ConstructTreeFormNode("Operator - assignment"));
     var_dec_treeForm->subnode("Identifier");
@@ -89,7 +98,7 @@ Parser getParser(Lexer lexer)
     myParser.addConstruct(var_dec_constr);
 
     // End
-    SP<Construct> end_constr(new Construct("End", TType::END, NType::END, [] (TokenManager& tm) { tm.exit = true; }, 0));
+    SP<Construct> end_constr(new Construct("End", TType::END, NType::END, [] (TokenManager& tm) { tm.exit = true; }, 0, false));
 
     // TODO: make this form of construct tree form the default
     SP<ConstructTreeFormNode> end_treeForm(new ConstructTreeFormNode("End"));
