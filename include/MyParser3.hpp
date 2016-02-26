dbfdfaa0586e95955462c9ea397b0f12c7f9e05a
M3579 <mihirkasmalkar@gmail.com>
2016-02-26 07:49:24 -0800

    Working on fixing issues that arised in KeyboardCodeLang compiler

--- include/MyParser3.hpp
+++ include/MyParser3.hpp
@@ -56,19 +56,19 @@ Parser getParser(Lexer lexer)
     Parser myParser(lexer);
 
     // let
-    SP<Construct> let_keyword(new Construct("Keyword - let", TType::Keyword::LET, NType::Keyword::LET, 0, 0, false));
+    SP<Construct> let_keyword(new Construct("Keyword - let", TType::Keyword::LET, NType::Keyword::LET, 0, 0));
 
     // <space>
-    SP<Construct> spaceAfterLet(new Construct("Space After Let", TType::SPACE, NType::SPACE, 0, [] (TokenManager& tm) { std::cout << "Error: expected space after let"; tm.exit = true; }, false));
+    SP<Construct> spaceAfterLet(new Construct("Space After Let", TType::SPACE, NType::SPACE, 0, [] (TokenManager& tm) { std::cout << "Error: expected space after let"; tm.exit = true; }));
 
     // let + <space>
     std::vector<SP<Construct>> letKeywordAndSpaceComponents {
         let_keyword, spaceAfterLet
     };
-    SP<Construct> letKeywordAndSpace(new Construct("Keyword - let + Space After Let", letKeywordAndSpaceComponents, 0, 0, true));
+    SP<Construct> letKeywordAndSpace(new Construct("Keyword - let + Space After Let", letKeywordAndSpaceComponents, 0, 0));
 
     // var
-    SP<Construct> identifier(new Construct("Identifier", TType::IDENTIFIER, NType::IDENTIFIER, 0, 0, false));
+    SP<Construct> identifier(new Construct("Identifier", TType::IDENTIFIER, NType::IDENTIFIER, 0, 0));
 
     // =
     std::vector<TokenType> var_dec_op_ttypes {
@@ -78,24 +78,24 @@ Parser getParser(Lexer lexer)
         NType::Operator::ASSIGNMENT_OP, NType::Operator::INCREMENT_OP
     };
 
-    SP<Construct> var_dec_op(new Construct("Var dec operator", var_dec_op_ttypes, var_dec_op_ntypes, 0, 0, false));
+    SP<Construct> var_dec_op(new Construct("Var dec operator", var_dec_op_ttypes, var_dec_op_ntypes, 0, 0));
     var_dec_op->notFound = [] (TokenManager& tm) {
         std::cout << "Error: expected operator after variable\n";
         tm.exit = true;
     };
 
     // value
-    SP<Construct> value(new Construct("Value", TType::Value::NUMBER, NType::Value::NUMBER, 0, 0, false));
+    SP<Construct> value(new Construct("Value", TType::Value::NUMBER, NType::Value::NUMBER, 0, 0));
 
     // <space>
-    SP<Construct> space(new Construct("Space", TType::SPACE, NType::SPACE, 0, 0, false));
+    SP<Construct> space(new Construct("Space", TType::SPACE, NType::SPACE, 0, 0));
 
     // Variable declaration statement
     std::vector<SP<Construct>> var_dec_components {
-        letKeywordAndSpace, identifier, space, var_dec_op, space, value
+        letKeywordAndSpace->getOptionalForm(), identifier, space, var_dec_op, space, value
     };
 
-    SP<Construct> var_dec_constr(new Construct("Variable Declaration", var_dec_components, 0, 0, false));
+    SP<Construct> var_dec_constr(new Construct("Variable Declaration", var_dec_components, 0, 0));
 
     SP<ConstructTreeFormNode> var_dec_treeForm(new ConstructTreeFormNode("Var dec operator"));
     var_dec_treeForm->subnode("Identifier");
@@ -106,7 +106,7 @@ Parser getParser(Lexer lexer)
     myParser.addConstruct(var_dec_constr);
 
     // End
-    SP<Construct> end_constr(new Construct("End", TType::END, NType::END, [] (TokenManager& tm) { tm.exit = true; }, 0, false));
+    SP<Construct> end_constr(new Construct("End", TType::END, NType::END, [] (TokenManager& tm) { tm.exit = true; }, 0));
 
     // TODO: make this form of construct tree form the default
     SP<ConstructTreeFormNode> end_treeForm(new ConstructTreeFormNode("End"));
